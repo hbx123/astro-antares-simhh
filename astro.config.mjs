096005ef368023c2958a50300fc8ad585bb1a166
coderxi1 <coderxi@qq.com>
2024-12-16 00:59:55 +0800

    feat:添加astro-icon依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import unocss from 'unocss/astro'
+import icon from "astro-icon";
 import { BASE_URL } from './src/config'
 import remarkDirective from 'remark-directive'
 import { remarkPlugins, rehypePlugins } from './src/plugins';
@@ -13,6 +14,7 @@ export default defineConfig({
     mdx(),
     sitemap({ filter: page => ['/tags/','/categories/'].map(PATH=>page.startsWith(BASE_URL+PATH)).includes(false) }),
     unocss({ injectReset: true, configFile: '/uno.config.ts' }),
+    icon()
   ],
   markdown: {
     remarkPlugins: [
