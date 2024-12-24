92d1ffe212a5984785f2e6dc1a73938ed1244795
coderxi1 <coderxi@qq.com>
2024-12-24 22:35:29 +0800

    pref:清晰化rehypeImageLazyLoad逻辑

--- plugins/rehypePlugins.ts
+++ plugins/rehypePlugins.ts
@@ -4,23 +4,20 @@ import template from './template'
 
 const rehypeImageLazyLoad: RehypePlugin = () => {
   return (tree) => {
-    let haveScript = false
+    let imageCount = 0
     visit(tree, (node) => {
       if (node.type == 'element' && node.tagName == 'img') {
-        node.properties['class'] = (
-          (node.properties['class'] || '') + ' lazyload'
-        ).trim()
+        node.properties['class'] = ((node.properties['class'] || '') + ' lazyload').trim()
         node.properties['data-src'] = node.properties['src']
         node.properties['src'] = ''
-        if (haveScript) return
-        haveScript = true
-        tree.children.push({
-          type: 'element',
-          tagName: 'script',
-          properties: { type: 'text/javascript' },
-          children: [{ type: 'text', value: template('image-lazy-load.js') }],
-        })
+        imageCount ++
       }
+    })
+    imageCount && tree.children.push({
+      type: 'element',
+      tagName: 'script',
+      properties: { type: 'text/javascript' },
+      children: [{ type: 'text', value: template('image-lazy-load.js') }],
     })
   }
 }
