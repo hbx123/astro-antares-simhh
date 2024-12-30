6dd787064ed678d64fc2ac4fac7a618698c16413
coderxi1 <coderxi@qq.com>
2024-12-30 17:24:27 +0800

    feat: 添加astro-pagefind插件并配置搜索功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import unocss from 'unocss/astro'
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 import expressiveCode from 'astro-expressive-code';
 import { BASE_URL } from './src/config'
 import { remarkPlugins, rehypePlugins } from './plugins';
@@ -16,6 +17,7 @@ export default defineConfig({
     sitemap({ filter: page => ['/tags/','/categories/'].map(PATH=>page.startsWith(BASE_URL+PATH)).includes(false) }),
     unocss({ injectReset: true, configFile: '/uno.config.ts' }),
     icon(),
+    pagefind(),
   ],
   markdown: {
     remarkPlugins,
